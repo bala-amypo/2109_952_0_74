7d98d2bb362aaab0be98bf6ce4719f384d1eed68
bala-amypo <bala@amypo.in>
2025-12-17 04:35:35 +0000

    User commit

--- entity/Student.java
+++ entity/Student.java
@@ -1,23 +1,52 @@
 package com.example.demo.entity;
-
-import java.time.LocalDate;
-
-public class Student {
+import java.time.*;
+public class Student{
     private Long id;
     private String name;
     private String dept;
     private LocalDate dob;
     private float cgpa;
-    public Long getid(){
+    public Student(){}
+     public Student(Long id,String name,String dept,LocalDate dob,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.dept=dept;
+        this.dob=dob;
+        this.cgpa=cgpa;
+    }
+   public void setId(Long id){
+         this.id=id;
+    }
+    public void setName(String name){
+         this.name=name;
+    }
+    public void setDept(String dept){
+         this.dept=dept;
+    }
+    public void setDob(LocalDate dob){
+         this.dob=dob;
+    }
+    public void setCgpa(float cgpa){
+          this.cgpa=cgpa;
+    }
+
+    public Long getId(){
         return id;
-    }
-    public void setid(Long id){
-        this.id=id;
-    }
-    public String getName(){
+    } 
+      public String getName(){
         return name;
-    }
-    public void setid(String name){
-        this.id=id;
-    }
+    } 
+      public String getDept(){
+        return dept;
+    } 
+      public LocalDate getDob(){
+        return dob;
+    } 
+      public float getCgpa(){
+        return cgpa;
+    } 
+
+
+
+
 }
